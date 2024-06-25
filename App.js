df688b97ca8cceb5ba1c65a8d38c146af1455f6b
muhriswan321 <105841104821@student.unismuh.ac.id>
2024-06-25 00:38:01 -0700

    Costum Komponen

--- App.js
+++ App.js
@@ -1,35 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TextInput } from 'react-native'
 import React from 'react'
+
+const ButtonCostum = ({ text, color }) => {
+  return (
+    <View style={{
+      backgroundColor: color,
+      width: 250,
+      height: 100,
+      borderRadius: 20,
+      justifyContent: 'center',
+      marginBottom: 10
+    }}>
+      <Text style={{
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 30,
+        fontWeight: 'bold',
+      }}>
+        {text}
+      </Text>
+    </View>
+  )
+}
 
 const App = () => {
   return (
     <View style={{
-      //justifyContent=>baris
-      //alignItems=>kolom
-      marginTop: 400
-      }}>
-      <View style={{
-        flex: 1, justifyContent: 'center', alignItems: 'flex-center', marginLeft: 25
-      }}>
-      <View style={{
-          width: 145, height: 40, backgroundColor: 'red', borderRadius: 15, justifyContent: 'center'
-      }}>
-          <Text style={{
-            fontSize: 20, color: 'white', textAlign: 'center', fontWeight: 'bold'
-          }}>Login</Text>
-      </View>  
-      </View>
-      <View style={{
-        flex: 1, justifyContent: 'center', alignItems: 'flex-end', marginRight: 25
-      }}>
-      <View style={{
-        width: 145, height: 40, backgroundColor: 'blue', borderRadius: 15, justifyContent: 'center'
-      }}>
-          <Text style={{
-            fontSize: 20, color: 'white', textAlign: 'center', fontWeight: 'bold'
-          }}>Sing Up</Text>
-      </View>
-      </View>
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      <ButtonCostum text= "LOGIN" color= "red" />
+      <ButtonCostum text= "LOGUOT" color= "green" />
+      <ButtonCostum text= "REGIS" color= "black" />
     </View>
   )
 }
